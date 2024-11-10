f3a708f72928db091c144827832bc95c8615820a
xuan <xatuzx2025@gmail.com>
2024-11-11 00:37:22 +0800

    11.11

--- main.go
+++ main.go
@@ -3,4 +3,11 @@ package main
 import "fmt" //+
 func main() {
 	fmt.Println("Hello, world!")
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if i*30+j*25 == 335 {
+				fmt.Println("30->", i, "25->", j)
+			}
+		}
+	}
 }
